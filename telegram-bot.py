db2713cf52f76b4e7ad6eafea6de24e1e02a1b34
ashkan0201 <ashkan02011@gmial.com>
2023-11-02 22:46:51 +0330

    added coment

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,15 +1,21 @@
 from random import randint
 import telebot
 
+# Connecting to the Telegram bot with the bot token.
 bot = telebot.TeleBot('YOUR-TOKEN')
 random_number = randint(0, 100)
 
+# Send the message when it is sent in the /start robot.
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Welcome to the Number Guessing Game! I'm thinking of a number between 0 and 100. Take a guess!")
 
+# Send your message by bot.
 @bot.message_handler(func=lambda message: True)
 def game(message):
+    """
+    Personal codes.
+    """
     try:
         input_user = int(message.text)
         if input_user < 0 or input_user > 100:
